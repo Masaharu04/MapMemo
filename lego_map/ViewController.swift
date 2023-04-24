5034e25d46d86a65d4b8b252f9b4223171b069d5
Masaharu04 <masaharu7532@icloud.com>
2023-04-25 01:41:24 +0900

    label declaration

--- lego_map/ViewController.swift
+++ lego_map/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 //Hello
+    
 
 }
 
