5820e5cb36d881aeaeea34580171bc4e7260b09f
Motoki-7 <kerokerokey0703@gmail.com>
2023-04-25 22:25:58 +0900

    [add]set_day,[edit]user_default

--- lego_map/user_default.swift
+++ lego_map/user_default.swift
@@ -5,20 +5,31 @@
 //  Created by Motoki on 2023/04/25.
 //
 
+/*-------------------------------------------------------------------------------------
+ --------userdefaultにデータを保存する---------
+ write_userdefault(マップのX座標(Double),マップのY座標(Double),メモの内容(String),日付(String),forkey(String))
+ --------userdefaultからデータを読み出す---------
+ read_userdefault(keyword(String)) -> 戻り値(マップのX座標(Double),マップのY座標(Double),メモの内容(String),日付(String))
+ --------userdefaultからデータを消す---------
+ delete_userdefault(keyword(String))
+ --------------------------------------------------------------------------------------*/
+
+
 import Foundation
 
-func write_userdefault(map_x: Int, map_y: Int, text: String, keyword: String){
-    let savedata = [String(map_x),String(map_y),text]
+func write_userdefault(map_x: Double, map_y: Double, text: String, date: String, keyword: String){
+    let savedata = [String(map_x), String(map_y), text, date]
     UserDefaults.standard.set(savedata, forKey: keyword)
 }
 
-func read_userdefault(keyword: String) -> (map_x: Int, map_y: Int,text: String){
-    let readdata = UserDefaults.standard.stringArray(forKey: keyword) ?? ["","","errer"]
-    let arg1:Int = Int(readdata[0]) ?? -1
-    let arg2:Int = Int(readdata[1]) ?? -1
+func read_userdefault(keyword: String) -> (map_x: Double, map_y: Double,text: String, date: String){
+    let readdata = UserDefaults.standard.stringArray(forKey: keyword) ?? ["","","errer","errer"]
+    let arg1:Double = Double(readdata[0]) ?? 0
+    let arg2:Double = Double(readdata[1]) ?? 0
     let arg3:String = String(readdata[2])
+    let arg4:String = String(readdata[3])
         
-    return (arg1,arg2,arg3)
+    return (arg1,arg2,arg3,arg4)
 }
 func delete_userdefault(keyword: String){
     UserDefaults.standard.removeObject(forKey: keyword)
