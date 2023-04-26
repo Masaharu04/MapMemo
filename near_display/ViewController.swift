c3976a1e171ffdf3d1d393a2b928e0f8bd8b54ef
A3426L <mikan7738@gmail.com>
2023-04-27 00:21:59 +0900

    Initial Commit

---
+++ near_display/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  near_display
+//
+//  Created by 山本聖留 on 2023/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
