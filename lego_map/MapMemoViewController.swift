53eca182302a821dc98e43d613e3fade8f07b4a2
Motoki-7 <kerokerokey0703@gmail.com>
2023-04-27 01:10:16 +0900

    edit

--- lego_map/MapMemoViewController.swift
+++ lego_map/MapMemoViewController.swift
@@ -19,14 +19,16 @@ class MapMemoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleTextField.text = saveData.object(forKey: "title")as? String
-        contentTextView.text = saveData.object(forKey: "content")as? String
+        let (map_x,map_y,title,text,date) = read_userdefault(keyword: "1")
+        titleTextField.text = title
+        contentTextView.text = text
         // Do any additional setup after loading the view.
     }
     @IBAction func saveMemo() {
+        let date = Date()
+        let st_date = date_to_string(date: date)
+        write_userdefault(map_x: 0, map_y: 0, title: titleTextField.text, text: contentTextView.text, date: st_date, keyword: "1")
         
-        saveData.set(titleTextField.text, forKey: "title")
-        saveData.set(contentTextView.text, forKey: "content")
         let alertController = UIAlertController(title: "保存", message: "保存完了", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alertController.addAction(okAction)
--- lego_map/user_default.swift
+++ lego_map/user_default.swift
@@ -7,9 +7,9 @@
 
 /*-------------------------------------------------------------------------------------
  --------userdefaultにデータを保存する---------
- write_userdefault(マップのX座標(Double),マップのY座標(Double),メモの内容(String),日付(String),forkey(String))
+ write_userdefault(マップのX座標(Double),マップのY座標(Double),タイトル(String),メモの内容(String),日付(String),keyword(String))
  --------userdefaultからデータを読み出す---------
- read_userdefault(keyword(String)) -> 戻り値(マップのX座標(Double),マップのY座標(Double),メモの内容(String),日付(String))
+ read_userdefault(keyword(String)) -> 戻り値(マップのX座標(Double),マップのY座標(Double),タイトル(String),メモの内容(String),日付(String))
  --------userdefaultからデータを消す---------
  delete_userdefault(keyword(String))
  --------------------------------------------------------------------------------------*/
@@ -17,20 +17,62 @@
 
 import Foundation
 
-func write_userdefault(map_x: Double, map_y: Double, text: String, date: String, keyword: String){
-    let savedata = [String(map_x), String(map_y), text, date]
+func write_userdefault(map_x: Double, map_y: Double, title: String!, text: String!, date: String, keyword: String){
+    let savedata = [String(map_x), String(map_y), title, text, date]
     UserDefaults.standard.set(savedata, forKey: keyword)
+    append_key(keyword: keyword)
 }
 
-func read_userdefault(keyword: String) -> (map_x: Double, map_y: Double,text: String, date: String){
-    let readdata = UserDefaults.standard.stringArray(forKey: keyword) ?? ["","","errer","errer"]
+func read_userdefault(keyword: String) -> (map_x: Double, map_y: Double, title: String, text: String, date: String){
+    let readdata = UserDefaults.standard.stringArray(forKey: keyword) ?? ["","","E","E","E"]
     let arg1:Double = Double(readdata[0]) ?? 0
     let arg2:Double = Double(readdata[1]) ?? 0
     let arg3:String = String(readdata[2])
     let arg4:String = String(readdata[3])
+    let arg5:String = String(readdata[4])
         
-    return (arg1,arg2,arg3,arg4)
+    return (arg1,arg2,arg3,arg4,arg5)
 }
 func delete_userdefault(keyword: String){
     UserDefaults.standard.removeObject(forKey: keyword)
+    delete_key(keyword: keyword)
+}
+
+func append_key(keyword: String){
+    var key_data = UserDefaults.standard.stringArray(forKey: "key_keyword") ?? []
+    if key_data.contains(keyword){
+        return
+    }
+    key_data.append(keyword)
+    UserDefaults.standard.set(key_data, forKey: "key_keyword")
+    print(key_data)
+}
+
+func delete_key(keyword: String){
+    var key_data = UserDefaults.standard.stringArray(forKey: "key_keyword") ?? []
+    key_data.removeAll(where: {$0 == keyword})
+    UserDefaults.standard.set(key_data, forKey: "key_keyword")
+    print(key_data)
+}
+
+func init_load() -> [Int: String]{
+    var dict_hash_key: [Int: String] = [:]
+    let key_data = UserDefaults.standard.stringArray(forKey: "key_keyword") ?? []
+    for key_buf in key_data {
+        let (map_x,map_y,title,text,date) = read_userdefault(keyword: key_buf)
+        
+        dict_hash_key[0/*hash*/] = key_buf
+    }
+    return dict_hash_key
+}
+
+func hash_to_keyword(hash: Int, dict_hash_key: [Int: String]) -> String{
+    let buf: String = dict_hash_key[hash] ?? ""
+    return buf
+}
+
+extension UserDefaults{
+    func removeAll(){
+        dictionaryRepresentation().forEach {removeObject(forKey: $0.key)}
+    }
 }
