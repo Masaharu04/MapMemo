5710fe95ec6f816de66c9cfa79c94fc7940c8533
A3426L <mikan7738@gmail.com>
2023-04-27 01:15:18 +0900

    [edit]

--- lego_map/ViewController.swift
+++ lego_map/ViewController.swift
@@ -12,10 +12,13 @@ import CoreLocation
 class ViewController: UIViewController ,CLLocationManagerDelegate, MKMapViewDelegate{
     
     @IBOutlet weak var mapView: MKMapView!
-    
+
     let locationManager = CLLocationManager()
-    var val_x :Double = -122.39
-    var val_y :Double = 37.768
+    var val_x :Double = -122.38
+    var val_y :Double = 37.76
+    var text_title:String = "test"
+    var text_subtitle:String = "test2"
+    var id_hash : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +32,8 @@ class ViewController: UIViewController ,CLLocationManagerDelegate, MKMapViewDele
         print("touchPin")
         print(val_y)
         print(val_x)
+
+
         addPin(latitude: val_y, longitude: val_x)
         
     }
@@ -37,7 +42,11 @@ class ViewController: UIViewController ,CLLocationManagerDelegate, MKMapViewDele
         print("getcurrentlocation")
         print(val_y)
         print(val_x)
+        text_title = "aiueo"
+        text_subtitle = "kakiku"
     }
+    
+    
     
     func addPin(latitude: Double, longitude: Double) {
         let coordinate = CLLocationCoordinate2DMake(latitude,longitude)
@@ -46,15 +55,14 @@ class ViewController: UIViewController ,CLLocationManagerDelegate, MKMapViewDele
         mapView.setRegion(region, animated:true)
         
         let pin = MKPointAnnotation()
-        pin.title = "現在地"
-        pin.subtitle = "yeah!"
+        pin.title = text_title
+        pin.subtitle = text_subtitle
         pin.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         print(pin.coordinate)
         mapView.addAnnotation(pin)
-        print(mapView.addAnnotation(pin))
-        //1<MKMarkerAnnotationView: 0x13c131000; frame = (0 0; 28 28); layer = <CALayer: 0x6000024f44a0>>
-        //2MKMarkerAnnotationView: 0x13e00cc00; frame = (0 0; 28 28); layer = <CALayer: 0x6000024e76a0>>
-        
+        id_hash = pin.hash
+        print(pin.hash)
+
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
@@ -69,42 +77,47 @@ class ViewController: UIViewController ,CLLocationManagerDelegate, MKMapViewDele
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
-        print("error: \(error.localizedDescription)")
-    }
-    
+            print("error: \(error.localizedDescription)")
+        }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
+
         if annotation.title == "My Location"{
             return nil
         }
         
-        
+
         let pinView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: nil)
         
         //pinView.image
-        
+
         pinView.canShowCallout = true
-        
+   
         let button = UIButton()
         button.frame = CGRect(x:0,y:0,width:60,height:70)
         button.setImage(UIImage(systemName: "trash.square"), for: .normal)
         button.tintColor = UIColor.red
-        
-        
+
+
         pinView.rightCalloutAccessoryView = button
         
         print(pinView)
-        
+            
         return pinView
     }
-    
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         guard let annotation = view.annotation else {
             return
         }
-        
+
         self.mapView.removeAnnotation(annotation)
     }
     
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        if let annotation = view.annotation{
+            print(annotation.title!!)
+            print(annotation.hash)
+        }
+    }
+
 }
