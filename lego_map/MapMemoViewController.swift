4967f996f71090143e311c6f3893483735601080
Masaharu04 <masaharu7532@icloud.com>
2023-04-28 12:40:50 +0900

    バックグランドローカル通知実装

--- lego_map/MapMemoViewController.swift
+++ lego_map/MapMemoViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import CoreLocation
+import UserNotifications
+
 
 class MapMemoViewController: UIViewController {
     
@@ -15,10 +18,10 @@ class MapMemoViewController: UIViewController {
     @IBOutlet var contentTextView: UITextView!
     
     var saveData: UserDefaults  = UserDefaults.standard
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         titleTextField.text = saveData.object(forKey: "title")as? String
         contentTextView.text = saveData.object(forKey: "content")as? String
         // Do any additional setup after loading the view.
@@ -35,33 +38,35 @@ class MapMemoViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
         
         //ローカル通知の設定
+        
         let content = UNMutableNotificationContent()
         content.title = "メモ発見！"
         content.body = "メモを除いてみよう"
         content.sound = UNNotificationSound.default
-
+        
         // 直ぐに通知を表示
+        sleep(10)
         let request = UNNotificationRequest(identifier: "immediately", content: content, trigger: nil)
         UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
         
     }
     
-
-    
-    
+    @IBAction func listButton(_ sender: UIButton) {
+        let content = UNMutableNotificationContent()
+        content.title = "ローカル通知"
+        content.body = "5秒後に表示されます。"
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: "localNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("ローカル通知の登録に失敗しました：", error)
+            } else {
+                print("ローカル通知を登録しました。")
+            }
+        }
+    }
     
 }
-
-  
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
